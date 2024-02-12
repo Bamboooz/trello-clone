e6d4049d6cced4628118c6879eac52a24676c578
XPenApp <xpeneditor@email.com>
2024-02-12 16:05:08 +0100

    test header

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,49 @@
-import React, { useState, useRef, useEffect, MouseEvent } from 'react';
+import React, { useState, useRef } from 'react';
+
+import { FiMenu } from "react-icons/fi";
 
 import { Node } from './components/node/node';
 import { NewNode } from './components/new-node/new-node';
 
-type NodeItems = { [idx: number]: string };
-type NodeListItem = { name: string; nodeItems: NodeItems };
-type NodeList = { [idx: number]: NodeListItem };
+type NodeList = { [idx: number]: { name: string, nodeItems: { [idx: number]: string }}};
 
 const App: React.FC = () => {
-    const [nodeList, setNodeList] = useState<NodeList>({
-        0: { name: "To-do", nodeItems: { 0: "0", 1: "1", 2: "2", 3: "3", 4: "4" } },
-        1: { name: "Currently doing", nodeItems: {} },
-        2: { name: "Done", nodeItems: {} },
-    });
+	const [nodeList, setNodeList] = useState<NodeList>({
+		0: { name: "To-do", nodeItems: { 0: "0", 1: "1", 2: "2", 3: "3", 4: "4" } },
+		1: { name: "Currently doing", nodeItems: {} },
+		2: { name: "Done", nodeItems: {} },
+	});
+	const windowScrollRef = useRef<HTMLDivElement>(null);
 
-    const windowScrollRef = useRef<HTMLDivElement>(null);
-    const [isMouseDown, setIsMouseDown] = useState(false);
-    const mouseCoords = useRef({
-        startX: 0,
-        scrollLeft: 0,
-    });
-
-    const handleDragStart = (e: MouseEvent) => {
-        if (!windowScrollRef.current) return;
-        const slider = windowScrollRef.current.children[0] as HTMLDivElement;
-        const startX = e.pageX - slider.offsetLeft;
-        const scrollLeft = slider.scrollLeft;
-        mouseCoords.current = { startX, scrollLeft };
-        setIsMouseDown(true);
-        document.body.style.cursor = "grabbing";
-    };
-
-    const handleDragEnd = () => {
-        setIsMouseDown(false);
-        if (!windowScrollRef.current) return;
-        document.body.style.cursor = "default";
-    };
-
-    const handleDrag = (e: MouseEvent) => {
-        if (!isMouseDown || !windowScrollRef.current) return;
-        e.preventDefault();
-        const slider = windowScrollRef.current.children[0] as HTMLDivElement;
-        const x = e.pageX - slider.offsetLeft;
-        const walkX = (x - mouseCoords.current.startX) * 1.5;
-        slider.scrollLeft = mouseCoords.current.scrollLeft - walkX;
-    };
-
-    const scrollToRight = () => {
+	const scrollToRight = () => {
         if (windowScrollRef.current) {
             const scrollWidth = windowScrollRef.current.scrollWidth;
             windowScrollRef.current.scrollLeft = scrollWidth ?? 0;
         }
     };
 
-    return (
-        <div
-            ref={windowScrollRef}
-            onMouseDown={handleDragStart}
-            onMouseUp={handleDragEnd}
-            onMouseMove={handleDrag}
-            className="App p-10 flex justify-start items-start gap-x-4"
-        >
-            {Object.keys(nodeList).map((index) => {
-                const idx = Number(index);
-                return <Node key={idx} nodeId={idx} nodeList={nodeList} setNodeList={setNodeList} />;
-            })}
+	return (
+		<>
+			<div className="App flex flex-col justify-start items-start">
+				<header className="flex items-center justify-between w-[100vw] sticky h-[60px] px-4 border-solid border-b-[1px] bg-slate-900 border-slate-700 shadow-2xl">
+					<p className="text-[16px] text-slate-200 font-semibold ml-10">My Board</p>
+					
+					<button className="p-2 rounded-sm hover:bg-slate-800">
+						<FiMenu className="text-slate-200" size={24} />
+					</button>
+				</header>
 
-            <NewNode setNodeList={setNodeList} scrollToRight={scrollToRight} />
-        </div>
-    );
+				<div ref={windowScrollRef} className="flex items-start justify-start p-10 gap-x-4 overflow-auto">
+					{Object.keys(nodeList).map((index) => {
+						const idx = Number(index);
+						return <Node key={idx} nodeId={idx} nodeList={nodeList} setNodeList={setNodeList} />;
+					})}
+
+					<NewNode setNodeList={setNodeList} scrollToRight={scrollToRight} />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default App;
